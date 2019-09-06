24e1cf5d1626c044d4b8e0f4b83613b23eddda3c
Amber Jones <amberlovescats14@gmail.com>
2019-09-06 11:53:30 -0500

    getters for scanner classes

--- src/com/amber/Interest.java
+++ src/com/amber/Interest.java
@@ -3,15 +3,28 @@ package com.amber;
 import java.util.Scanner;
 
 public class Interest {
-    public static double askInterest(){
+
+    double monthlyInterest;
+
+    // CONSTRUCTOR -- not really needed because Scanner is generating an input
+    public Interest(double monthlyInterest) {
+        this.monthlyInterest = monthlyInterest;
+    }
+
+    //SET
+    public double askInterest(){
         Scanner scan = new Scanner(System.in);
-        double monthlyInterest;
         while(true){
             System.out.print("Annual Interest?");
-            monthlyInterest = (scan.nextDouble() / 100) /12;
+            this.monthlyInterest = (scan.nextDouble() / 100) /12;
             if(monthlyInterest > 0 && monthlyInterest < 10) break;
             System.out.println("Please enter a rate between .1 - 10");
         }
         return  monthlyInterest;
+    }
+
+    //GET
+    public double getMonthlyInterest() {
+        return monthlyInterest;
     }
 }
--- src/com/amber/Years.java
+++ src/com/amber/Years.java
@@ -3,16 +3,29 @@ package com.amber;
 import java.util.Scanner;
 
 public class Years {
-    public static int askYears() {
+    private int months;
+
+    // Constructor  -- not really needed because the scanner is happening here
+    public Years(int months) {
+        this.months = months;
+    }
+
+    public int askYears() {
         Scanner scan = new Scanner(System.in);
-        int months;
         while(true) {
             System.out.print("How many years would you like to finance?");
             int years = scan.nextInt();
-            months = years * 12;
+            this.months = years * 12;
             if(years > 10 && years < 51) break;
             System.out.println("Please choose between 10 - 50 years.");
         }
         return months;
     }
+
+
+// GET
+    public int getMonths() {
+        return  months;
+    }
+
 }
